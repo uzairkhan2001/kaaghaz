8d9bb4a97576a96662f864de7c8d3ed7e14931c5
uzairkhan2001 <khanuzair200101@gmail.com>
2023-01-03 00:28:33 +0500

    Bug fix!

--- paper/src/mainpkg/PlaceOrder.java
+++ paper/src/mainpkg/PlaceOrder.java
@@ -6,12 +6,12 @@ import java.sql.DriverManager;
 import java.sql.Statement;
 
 public class PlaceOrder {
-	   public void createPurchaseOrder(int SizeID, int WeightID, int VendorID, int Quantity) {
+	   public void createPurchaseOrder(int rawID,int SizeID, int WeightID, int VendorID, int Quantity) {
 	    	try {
 				Class.forName("com.mysql.jdbc.Driver");
 				Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/fyp","root","");
 				Statement stmt=con.createStatement();
-				 stmt.executeUpdate("insert into purchaseorder (WeightID,SizeID,VendorID,Quantity,Time) VALUES('"+WeightID+"','"+SizeID+"','"+VendorID+"','"+Quantity+"',now())");
+				 stmt.executeUpdate("insert into purchaseorder (rawmaterial_ID,WeightID,SizeID,VendorID,Quantity,Time) VALUES('"+rawID+"','"+WeightID+"','"+SizeID+"','"+VendorID+"','"+Quantity+"',now())");
 				  con.close();
 			} catch (Exception e) {
 				e.printStackTrace();
