68e9aca5781a252f6e555c59c7e7e6c99d3cd567
uzairkhan2001 <khanuzair200101@gmail.com>
2023-02-21 23:12:33 +0500

    In process!

--- paper/src/mainpkg/SendForPrintingJob.java
+++ paper/src/mainpkg/SendForPrintingJob.java
@@ -5,12 +5,12 @@ import java.sql.DriverManager;
 import java.sql.Statement;
 
 public class SendForPrintingJob {
-	public String sendPrintingJob(int purchaseID, int qty) {
+	public String sendPrintingJob(int purchaseID,String vendor, int qty) {
     	try {
 			Class.forName("com.mysql.jdbc.Driver");
 			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/fyp","root","");
 			Statement stmt=con.createStatement();
-			stmt.executeUpdate("INSERT INTO `sendforprinting`(`PID`, `quantity`,`Time`) VALUES ("+purchaseID+","+qty+",now())");
+			stmt.executeUpdate("INSERT INTO `sendforprinting`(`PID`,`printVendor`, `quantity`,`Time`) VALUES ("+purchaseID+","+vendor+","+qty+",now())");
 			con.close();
 			return "Sent Successfully!";
 		} catch (Exception e) {
