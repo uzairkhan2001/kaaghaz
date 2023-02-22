1b825e04027a19eb2064e55147a5657a53947462
aakhtar4 <16030059@lums.edu.pk>
2023-02-22 17:38:01 +0500

    New database script from Uzair

--- fyp.sql
+++ fyp.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Feb 21, 2023 at 05:47 AM
--- Server version: 5.7.36
--- PHP Version: 7.4.26
+-- Host: 127.0.0.1
+-- Generation Time: Feb 21, 2023 at 06:49 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,32 +20,62 @@ SET time_zone = "+00:00";
 --
 -- Database: `fyp`
 --
-CREATE DATABASE IF NOT EXISTS `fyp` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `fyp`;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `paperinventory`
+-- Table structure for table `catalog`
 --
 
-DROP TABLE IF EXISTS `paperinventory`;
-CREATE TABLE IF NOT EXISTS `paperinventory` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `catalog` (
+  `ID` int(11) NOT NULL,
+  `rawmaterial_ID` int(4) NOT NULL,
+  `VendorID` int(11) NOT NULL,
+  `SizeID` int(11) NOT NULL,
+  `WeightID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `catalog`
+--
+
+INSERT INTO `catalog` (`ID`, `rawmaterial_ID`, `VendorID`, `SizeID`, `WeightID`) VALUES
+(1, 1, 4, 5, 4),
+(2, 1, 2, 5, 4),
+(3, 1, 4, 2, 5),
+(4, 1, 3, 1, 4),
+(5, 1, 2, 3, 2),
+(6, 1, 3, 3, 2),
+(7, 1, 3, 2, 2),
+(8, 1, 1, 3, 5),
+(9, 1, 4, 1, 3),
+(10, 1, 5, 5, 4),
+(11, 2, 4, 3, 7),
+(12, 3, 1, 5, 6),
+(13, 3, 2, 1, 8),
+(14, 2, 4, 4, 7);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `inventory`
+--
+
+CREATE TABLE `inventory` (
+  `ID` int(11) NOT NULL,
   `SizeID` int(11) NOT NULL,
   `WeightID` int(11) NOT NULL,
   `VendorID` int(11) NOT NULL,
   `Quantity` int(11) NOT NULL,
   `QuantityDamage` int(11) NOT NULL,
-  `Time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4;
+  `Time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `paperinventory`
+-- Dumping data for table `inventory`
 --
 
-INSERT INTO `paperinventory` (`ID`, `SizeID`, `WeightID`, `VendorID`, `Quantity`, `QuantityDamage`, `Time`) VALUES
+INSERT INTO `inventory` (`ID`, `SizeID`, `WeightID`, `VendorID`, `Quantity`, `QuantityDamage`, `Time`) VALUES
 (1, 5, 4, 4, 1220, 15, '2022-06-22 05:06:16'),
 (2, 5, 4, 2, 1500, 100, '2022-06-02 05:24:19'),
 (3, 2, 5, 4, 2500, 20, '2022-06-01 05:46:50'),
@@ -60,145 +90,69 @@ INSERT INTO `paperinventory` (`ID`, `SizeID`, `WeightID`, `VendorID`, `Quantity`
 -- --------------------------------------------------------
 
 --
--- Table structure for table `paperpurchaseorder`
+-- Table structure for table `printingvendor`
 --
 
-DROP TABLE IF EXISTS `paperpurchaseorder`;
-CREATE TABLE IF NOT EXISTS `paperpurchaseorder` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `printingvendor` (
+  `ID` int(40) NOT NULL,
+  `name` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `printingvendor`
+--
+
+INSERT INTO `printingvendor` (`ID`, `name`) VALUES
+(1, 'PEC'),
+(2, 'PRO'),
+(3, 'PQM'),
+(4, 'PLN');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `purchaseorder`
+--
+
+CREATE TABLE `purchaseorder` (
+  `ID` int(11) NOT NULL,
+  `rawmaterial_ID` int(4) NOT NULL,
   `SizeID` int(11) NOT NULL,
   `WeightID` int(11) NOT NULL,
   `VendorID` int(11) NOT NULL,
   `Quantity` int(11) NOT NULL,
-  `Time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4;
+  `Time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `paperpurchaseorder`
+-- Dumping data for table `purchaseorder`
 --
 
-INSERT INTO `paperpurchaseorder` (`ID`, `SizeID`, `WeightID`, `VendorID`, `Quantity`, `Time`) VALUES
-(2, 5, 4, 4, 1250, '2022-10-17 10:53:08'),
-(3, 1, 3, 4, 1350, '2022-10-17 10:53:08'),
-(4, 3, 5, 1, 11234, '2022-10-17 10:53:08'),
-(5, 1, 4, 3, 1555, '2022-10-17 10:53:08'),
-(6, 1, 3, 4, 11290, '2022-10-17 10:53:08'),
-(7, 2, 2, 3, 122222, '2022-10-17 10:53:08'),
-(8, 1, 4, 3, 19876, '2022-10-17 10:53:08'),
-(9, 3, 5, 1, 14444, '2022-10-17 10:53:08'),
-(10, 3, 2, 3, 153, '2022-10-17 10:53:08'),
-(11, 2, 5, 4, 12356789, '2022-10-17 10:53:08'),
-(12, 1, 4, 3, 12423, '2022-10-17 10:53:08'),
-(13, 1, 4, 3, 1250, '2022-10-17 10:53:08'),
-(14, 3, 5, 1, 11234, '2022-10-17 10:53:08'),
-(15, 1, 4, 3, 1100, '2022-10-17 10:53:08'),
-(16, 3, 2, 2, 1199, '2022-10-17 10:53:08'),
-(17, 3, 2, 2, 4323, '2022-12-04 09:57:35'),
-(18, 2, 5, 4, 20, '2022-12-13 11:10:05');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `papersize`
---
-
-DROP TABLE IF EXISTS `papersize`;
-CREATE TABLE IF NOT EXISTS `papersize` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `Size` varchar(45) NOT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `papersize`
---
-
-INSERT INTO `papersize` (`ID`, `Size`) VALUES
-(1, '23 * 36inch'),
-(2, '25 * 35inch'),
-(3, '19 * 32inch'),
-(4, '25 * 47inch'),
-(5, '17 * 35inch');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `papervendor`
---
-
-DROP TABLE IF EXISTS `papervendor`;
-CREATE TABLE IF NOT EXISTS `papervendor` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `Name` varchar(45) NOT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `papervendor`
---
-
-INSERT INTO `papervendor` (`ID`, `Name`) VALUES
-(1, 'IK'),
-(2, 'IBM'),
-(3, 'ORC'),
-(4, 'GEO'),
-(5, 'LGH');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `papervendorcatalog`
---
-
-DROP TABLE IF EXISTS `papervendorcatalog`;
-CREATE TABLE IF NOT EXISTS `papervendorcatalog` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `VendorID` int(11) NOT NULL,
-  `SizeID` int(11) NOT NULL,
-  `WeightID` int(11) NOT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `papervendorcatalog`
---
-
-INSERT INTO `papervendorcatalog` (`ID`, `VendorID`, `SizeID`, `WeightID`) VALUES
-(1, 4, 5, 4),
-(2, 2, 5, 4),
-(3, 4, 2, 5),
-(4, 3, 1, 4),
-(5, 2, 3, 2),
-(6, 3, 3, 2),
-(7, 3, 2, 2),
-(8, 1, 3, 5),
-(9, 4, 1, 3),
-(10, 5, 5, 4);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `paperweight`
---
-
-DROP TABLE IF EXISTS `paperweight`;
-CREATE TABLE IF NOT EXISTS `paperweight` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `Weight` varchar(45) NOT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `paperweight`
---
-
-INSERT INTO `paperweight` (`ID`, `Weight`) VALUES
-(1, '70 GSM'),
-(2, '85 GSM'),
-(3, '105 GSM'),
-(4, '80 GSM'),
-(5, '72 GSM');
+INSERT INTO `purchaseorder` (`ID`, `rawmaterial_ID`, `SizeID`, `WeightID`, `VendorID`, `Quantity`, `Time`) VALUES
+(3, 1, 1, 3, 4, 1350, '2022-12-30 18:20:46'),
+(4, 1, 3, 5, 1, 11234, '2022-12-30 18:20:55'),
+(5, 1, 1, 4, 3, 1555, '2022-12-30 18:21:03'),
+(6, 1, 1, 3, 4, 11290, '2022-12-30 18:21:13'),
+(7, 1, 2, 2, 3, 122222, '2022-12-30 18:21:23'),
+(8, 1, 1, 4, 3, 19876, '2022-12-30 18:21:32'),
+(9, 1, 3, 5, 1, 14444, '2022-12-30 18:21:49'),
+(10, 1, 3, 2, 3, 153, '2022-12-30 18:21:59'),
+(11, 1, 2, 5, 4, 12356789, '2022-12-30 18:22:09'),
+(12, 1, 1, 4, 3, 12423, '2022-12-30 18:22:17'),
+(13, 1, 1, 4, 3, 1250, '2022-12-30 18:22:27'),
+(14, 1, 3, 5, 1, 11234, '2022-12-30 18:22:37'),
+(15, 1, 1, 4, 3, 1100, '2022-12-30 18:22:46'),
+(16, 1, 3, 2, 2, 1199, '2022-12-30 18:22:55'),
+(17, 1, 3, 2, 2, 4323, '2022-12-30 18:23:08'),
+(18, 0, 3, 2, 2, 1200, '2022-12-31 19:11:27'),
+(19, 0, 5, 6, 1, 1234, '2022-12-31 19:12:22'),
+(20, 1, 1, 2, 3, 100, '2023-01-02 18:03:50'),
+(21, 1, 1, 2, 3, 100, '2023-01-02 18:04:04'),
+(22, 2, 3, 2, 1, 100, '2023-01-02 18:04:46'),
+(23, 3, 1, 3, 4, 2000, '2023-01-02 18:11:23'),
+(24, 2, 3, 2, 3, 35000, '2023-01-22 18:51:55'),
+(25, 1, 2, 2, 3, 999, '2023-01-23 05:43:02'),
+(26, 1, 2, 5, 4, 350, '2023-01-23 07:01:04');
 
 -- --------------------------------------------------------
 
@@ -206,12 +160,10 @@ INSERT INTO `paperweight` (`ID`, `Weight`) VALUES
 -- Table structure for table `rawmaterialtype`
 --
 
-DROP TABLE IF EXISTS `rawmaterialtype`;
-CREATE TABLE IF NOT EXISTS `rawmaterialtype` (
-  `RawMaterialId` int(11) NOT NULL AUTO_INCREMENT,
-  `RawMaterialName` varchar(45) NOT NULL,
-  PRIMARY KEY (`RawMaterialId`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `rawmaterialtype` (
+  `RawMaterialId` int(11) NOT NULL,
+  `RawMaterialName` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `rawmaterialtype`
@@ -219,8 +171,32 @@ CREATE TABLE IF NOT EXISTS `rawmaterialtype` (
 
 INSERT INTO `rawmaterialtype` (`RawMaterialId`, `RawMaterialName`) VALUES
 (1, 'Paper'),
-(2, 'Cardboard'),
-(3, 'Wire');
+(2, 'Title Card'),
+(3, 'Separator Card');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `receiveprinting`
+--
+
+CREATE TABLE `receiveprinting` (
+  `ID` int(40) NOT NULL,
+  `printID` int(40) NOT NULL,
+  `receivequan` int(40) NOT NULL,
+  `time` timestamp(6) NOT NULL DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `receiveprinting`
+--
+
+INSERT INTO `receiveprinting` (`ID`, `printID`, `receivequan`, `time`) VALUES
+(1, 3, 0, '2023-02-20 17:34:40.712285'),
+(2, 5, 0, '2023-02-20 17:34:58.530232'),
+(3, 25, 0, '2023-02-20 17:35:13.279701'),
+(4, 26, 0, '2023-02-20 17:35:25.877020'),
+(5, 25, 0, '2023-02-20 17:35:38.066924');
 
 -- --------------------------------------------------------
 
@@ -228,15 +204,13 @@ INSERT INTO `rawmaterialtype` (`RawMaterialId`, `RawMaterialName`) VALUES
 -- Table structure for table `recieveorder`
 --
 
-DROP TABLE IF EXISTS `recieveorder`;
-CREATE TABLE IF NOT EXISTS `recieveorder` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `recieveorder` (
+  `ID` int(11) NOT NULL,
   `Purchase ID` int(11) NOT NULL,
   `Receive Intact` int(11) NOT NULL,
   `Receive Damage` int(11) NOT NULL,
-  `Time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=115 DEFAULT CHARSET=utf8mb4;
+  `Time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `recieveorder`
@@ -334,7 +308,252 @@ INSERT INTO `recieveorder` (`ID`, `Purchase ID`, `Receive Intact`, `Receive Dama
 (111, 15, 1000, 5, '2022-10-17 11:11:01'),
 (112, 3, 2, 2, '2022-12-04 09:57:35'),
 (113, 0, 0, 0, '2022-12-04 10:21:00'),
-(114, 2, 5, 4, '2022-12-13 11:10:05');
+(114, 0, 0, 0, '2022-12-13 10:18:58'),
+(115, 3, 2, 2, '2022-12-31 19:11:27'),
+(116, 5, 6, 1, '2022-12-31 19:12:22'),
+(117, 16, 500, 99, '2022-12-31 19:17:36'),
+(118, 1, 3, 4, '2023-01-02 18:11:23'),
+(119, 3, 2, 3, '2023-01-22 18:51:55'),
+(120, 2, 2, 3, '2023-01-23 05:43:02'),
+(121, 25, 80, 910, '2023-01-23 05:44:30'),
+(122, 2, 5, 4, '2023-01-23 07:01:04'),
+(123, 17, 4323, 0, '2023-01-23 07:02:24'),
+(124, 26, 250, 10, '2023-01-23 07:03:17'),
+(125, 26, 90, 0, '2023-01-23 07:04:06');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sendforprinting`
+--
+
+CREATE TABLE `sendforprinting` (
+  `ID` int(40) NOT NULL,
+  `PID` int(40) NOT NULL,
+  `printVendor` varchar(40) NOT NULL,
+  `quantity` int(40) NOT NULL,
+  `time` timestamp(6) NOT NULL DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `sendforprinting`
+--
+
+INSERT INTO `sendforprinting` (`ID`, `PID`, `printVendor`, `quantity`, `time`) VALUES
+(1, 3, '2', 1234, '2023-02-20 17:51:40.897454'),
+(2, 5, '4', 4321, '2023-02-20 17:52:20.429984'),
+(3, 25, '1', 2, '2023-02-20 17:52:20.456468'),
+(4, 26, '3', 90, '2023-02-20 17:52:20.473386'),
+(5, 25, '2', 1234567, '2023-02-20 17:52:20.490427');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `size`
+--
+
+CREATE TABLE `size` (
+  `ID` int(11) NOT NULL,
+  `Size` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `size`
+--
+
+INSERT INTO `size` (`ID`, `Size`) VALUES
+(1, '23 * 36inch'),
+(2, '25 * 35inch'),
+(3, '19 * 32inch'),
+(4, '25 * 47inch'),
+(5, '17 * 35inch');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vendor`
+--
+
+CREATE TABLE `vendor` (
+  `ID` int(11) NOT NULL,
+  `Name` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `vendor`
+--
+
+INSERT INTO `vendor` (`ID`, `Name`) VALUES
+(1, 'IK'),
+(2, 'IBM'),
+(3, 'ORC'),
+(4, 'GEO'),
+(5, 'LGH');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `weight`
+--
+
+CREATE TABLE `weight` (
+  `ID` int(11) NOT NULL,
+  `Weight` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `weight`
+--
+
+INSERT INTO `weight` (`ID`, `Weight`) VALUES
+(1, '70 GSM'),
+(2, '85 GSM'),
+(3, '105 GSM'),
+(4, '80 GSM'),
+(5, '72 GSM'),
+(6, '115 GSM'),
+(7, '120 GSM'),
+(8, '130 GSM');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `catalog`
+--
+ALTER TABLE `catalog`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `inventory`
+--
+ALTER TABLE `inventory`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `printingvendor`
+--
+ALTER TABLE `printingvendor`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `purchaseorder`
+--
+ALTER TABLE `purchaseorder`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `rawmaterialtype`
+--
+ALTER TABLE `rawmaterialtype`
+  ADD PRIMARY KEY (`RawMaterialId`);
+
+--
+-- Indexes for table `receiveprinting`
+--
+ALTER TABLE `receiveprinting`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `recieveorder`
+--
+ALTER TABLE `recieveorder`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `sendforprinting`
+--
+ALTER TABLE `sendforprinting`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `size`
+--
+ALTER TABLE `size`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `vendor`
+--
+ALTER TABLE `vendor`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `weight`
+--
+ALTER TABLE `weight`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `catalog`
+--
+ALTER TABLE `catalog`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `inventory`
+--
+ALTER TABLE `inventory`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `printingvendor`
+--
+ALTER TABLE `printingvendor`
+  MODIFY `ID` int(40) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `purchaseorder`
+--
+ALTER TABLE `purchaseorder`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
+--
+-- AUTO_INCREMENT for table `rawmaterialtype`
+--
+ALTER TABLE `rawmaterialtype`
+  MODIFY `RawMaterialId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `receiveprinting`
+--
+ALTER TABLE `receiveprinting`
+  MODIFY `ID` int(40) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `recieveorder`
+--
+ALTER TABLE `recieveorder`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=126;
+
+--
+-- AUTO_INCREMENT for table `sendforprinting`
+--
+ALTER TABLE `sendforprinting`
+  MODIFY `ID` int(40) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `size`
+--
+ALTER TABLE `size`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `vendor`
+--
+ALTER TABLE `vendor`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `weight`
+--
+ALTER TABLE `weight`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
